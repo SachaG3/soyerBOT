2bba06552c2e639c87435b89192f271cd7e90938
Sacha Guignard <sacha.guignard@caensup.fr>
2024-02-10 19:21:44 +0100

    probléme avec la mise à jour du score

--- profilePartage.py
+++ profilePartage.py
@@ -9,7 +9,8 @@ class profil:
             if z == 1:
                 return f"Ton score est : {current_score}"
             else:
-                    return "Score mis à jour avec succès."
+                utilisateurRepository.update_score(id, z)
+                return "Score mis à jour avec succès."
         except Exception as e:
             print(e)
             return "Une erreur est survenue lors de l'accès à la base de données."
